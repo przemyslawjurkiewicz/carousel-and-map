e9549583c3e985704b798bd5133c64c3ea0a9aa7
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-09 19:16:03 +0100

    Add map

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict';
-(function () {
+(function() {
   var templateItem = document.getElementById('template-image-list').innerHTML;
   var elem = document.querySelector('.carousel');
 
   Mustache.parse(templateItem);
   for (var i = 0; i < imagesData.length; i++) {
     elem.innerHTML += Mustache.render(templateItem, imagesData[i]);
-  };
+  }
 
   var flkty = new Flickity(elem, {
     // options
@@ -17,27 +17,26 @@
     hash: true
   });
 
-  document.querySelectorAll('.rewind').forEach(function (e) {
-    e.addEventListener('click', function () {
+  document.querySelectorAll('.rewind').forEach(function(e) {
+    e.addEventListener('click', function() {
       flkty.select(0);
     });
   });
 
-  flkty.on('scroll', function (progress) {
+  flkty.on('scroll', function(progress) {
     progress = Math.max(0, Math.min(1, progress));
     document.querySelector('.progress-bar').style.width = progress * 100 + '%';
   });
 
   window.initMap = function() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = { lat: -25.363, lng: 131.044 };
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 4,
-            center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-            map: map
-        }); 
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
   };
-
 })();
