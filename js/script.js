d91f7d7f89af94ad22561da482deea68cd0a9834
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-09 10:36:48 +0100

    Add rewindButton & progressBar

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 'use strict';
-(function() {
-var elem = document.querySelector('.carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  pageDots: false,
-  lazyLoad: 1,
-  hash: true
-});
+(function () {
+
+  var elem = document.querySelector('.carousel');
+  var rewind = document.querySelectorAll('.rewind');
+  var progressBar = document.querySelector('.progress-bar')
+
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    pageDots: false,
+    lazyLoad: 1,
+    hash: true
+  });
+
+  rewind.forEach(function (e) {
+    e.addEventListener('click', function () {
+      flkty.select(0);
+    })
+  });
+
+  flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+  });
 
 })();
